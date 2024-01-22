08962c3a9503810a624fa0f8a64e94c34777e5a1
edscodes <edscodes@gmail.com>
2024-01-22 00:19:22 -0500

    Se utiliza la P.O.O. para mejorar la manera tecnica de registrar cada cliente, ademas se mejora con ello la manera de presentar la info recopilada y presentada a partir de lo insertado en el formulario.

--- js/script.js
+++ js/script.js
@@ -5,35 +5,49 @@
 
 // ---Formulario de registro:
 
+// funcion constructora de clientes (objetos)
+function nuevoCliente (nombre, email, telefono, direccion, contrasenaRegistro, genero, fechaNacimiento, pais, aceptaTerminos){
+    this.nombre = nombre
+    this.email = email
+    this.telefono = telefono
+    this.direccion = direccion
+    this.contrasenaRegistro = contrasenaRegistro
+    this.genero = genero
+    this.fechaNacimiento = fechaNacimiento
+    this.pais = pais
+    this.aceptaTerminos = aceptaTerminos
+};
+
 // Variables globales para contar los id de clientes registrados, sus email y contraseña de ingreso sesion:
-var idCliente = [];
-var email = [];
-var contrasenaRegistro = [];
+var idCliente = 0;
 var validacionUsuario = false;
 var opcionPremioSorp = 0;
 
 //reseteo del formulario al final del registro de cada cliente
 function resetFormulario() {
     document.querySelector('.formularioRegistro').reset();
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('registrarBtn').addEventListener('click', function(event) {
         event.preventDefault();
 
-        var nombre = document.querySelector('.nombreRegistro').value;
-        email = document.querySelector('[name="mailRegistro"]').value;
-        var telefono = document.querySelector('[name="phone"]').value;
-        contrasenaRegistro = document.querySelector('[name="regPassword"]').value
-        var genero = document.querySelector('[name="gender"]:checked').value;
-        var fechaNacimiento = document.querySelector('[name="fechaNac"]').value;
-        var pais = document.querySelector('[name="paisRegistro"]').value;
-        var aceptaTerminos = document.querySelector('[name="terminos"]').checked;
+        var cliente = new nuevoCliente (
+            nombre = document.querySelector('.nombreRegistro').value, 
+            email = document.querySelector('[name="mailRegistro"]').value, 
+            telefono = document.querySelector('[name="phone"]').value, 
+            direccion = document.querySelector('[name="direccionCliente"]').value,
+            contrasenaRegistro = document.querySelector('[name="regPassword"]').value, 
+            genero = document.querySelector('[name="gender"]:checked').value, 
+            fechaNacimiento = document.querySelector('[name="fechaNac"]').value, 
+            pais = document.querySelector('[name="paisRegistro"]').value, 
+            aceptaTerminos = document.querySelector('[name="terminos"]').checked
+        );
 
         if (!aceptaTerminos) {
             alert("Debes aceptar los términos y condiciones para continuar.");
             return; // Detiene la ejecución si no se aceptan los términos
-        }
+        };
 
         // Calcular edad del usuario
         var fechaNacUsuario = new Date(fechaNacimiento);
@@ -61,52 +75,49 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log("Registro & id no válidos");
             validacionUsuario == false
             return;
-        }
+        };
 
         //validar el registro ok (true), y mediante una funcion de aletoriedad calcular uno de los 4 beneficios ofrecidos, mostrandolo en un alert en la pagina al cliente:
         function aleatorio(min, max){
             return Math.floor(Math.random() * (max-min+1) + min);
-        }
+        };
 
         do {
             opcionPremioSorp = aleatorio(1,4)
             switch (opcionPremioSorp) {
                 case 1:
                     alert("¡Bienvenid@! haz ganado un bono de bienvenida del 15% de descuento para tu primera compra.");
+                    console.log("Sorpresa ganada: " + opcionPremioSorp + ". Bono de bienvenida del 15% de descuento para la 1era compra.");
                     break;
                 case 2:
                     alert("¡Bienvenid@! el envío de tu primera compra es totalmente gratuito.");
+                    console.log("Sorpresa ganada: " + opcionPremioSorp + ". El envío de la primera compra es totalmente gratuito.");
                     break;
                 case 3:
                     alert("¡Bienvenid@! haz ganado un cupón único del 10% adicional para tu siguiente compra.");
+                    console.log("Sorpresa ganada: " + opcionPremioSorp + ". Cupón único del 10% adicional para la siguiente compra.");
                     break;
                 case 4:
                     alert("¡Bienvenid@! para pedidos mayores a $200.000, incluiremos un fabuloso regalo sorpresa en tu paquete.");
+                    console.log("Sorpresa ganada: " + opcionPremioSorp + ". Para pedidos mayores a $200.000, un regalo sorpresa.");
                     break;
             }
             break;
         } while (validacionUsuario == true);
-
-        // Mostrar los datos recopilados en el formulario (se que en la vida real no seria seguro, pero mientras aprendemos lo hago como ejercicio :) )
-        console.log("Nombre: " + nombre);
-        console.log("Email: " + email);
-        console.log("Teléfono: " + telefono);
-        console.log("Contraseña: " + contrasenaRegistro);
-        console.log("Género: " + genero);
-        console.log("Fecha de Nacimiento: " + fechaNacimiento);
-        console.log("Edad: " + edadUsuario);
-        console.log("País: " + pais);
-        console.log("Acepta Términos: " + aceptaTerminos);
-        console.log("Regalo obtenido: " + opcionPremioSorp);
-
-        // Imprimo línea al final para separar cada registro en el console.log
-        console.log("<----------------------------------------------->");
-        
         resetFormulario();
+
+        // Imprimir propiedades y valores del cliente registrado
+        for (var propiedad in cliente) {
+        if (cliente.hasOwnProperty(propiedad)) {
+            console.log(propiedad + ": " + cliente[propiedad]);
+        };
 
         //Funcion del boton de borrar datos
         document.getElementById('borrarDatosBtn').addEventListener('click', resetFormulario);
-    });
+    };
+    
+    // Imprimo línea al final para separar cada registro en el console.log
+    console.log("<----------------------------------->");
 
     // ---Formulario de Inicio de sesion:
 
@@ -127,5 +138,4 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
-
+});
