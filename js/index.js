ea06684a570a15c26f75f5f884233bf095f8147e
edscodes <edscodes@gmail.com>
2024-03-03 17:18:05 -0500

    se modifica los arrays de productos por APIS locales para ponerlo en practica, se usan async functions y promesas para optimizar el desempeño de consumo de las apis antes que el codigo se ejecute

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     
-    const productos = [
-        {id:1, imagenUrl: "./multi/mujer_con_jean_roto.jpeg", textoProducto: "Jean Dama", descuento: "-10%", talla: "Tallas 26-36", precio: 75000, color: "Azul", genero: "mujer"},
-        {id:2, imagenUrl: "./multi/mujer_blusa_beige.jpeg", textoProducto: "Blusa dama", descuento: "-10%", talla: "Tallas S-XL", precio: 65000,  color: "Beige", genero: "mujer"},
-        {id:3, imagenUrl: "./multi/saco_gris_hombre.jpeg", textoProducto: "Saco gris", descuento: "-10%", talla: "Tallas S-XL", precio: 70000,  color: "Gris", genero: "hombre"},
-        {id:4, imagenUrl: "./multi/buzo_rojo_hombre.jpeg", textoProducto: "Buzo hombre", descuento: "-10%", talla: "Tallas S-XL", precio: 90000,  color: "Rojo", genero: "hombre"}
-    ];
+    try {
+        async function consumirApiProductos() {
+            try {
+                const respuesta = await fetch('./js/productos.json');
+                const datos = await respuesta.json();
+                return datos;
+            } catch (error) {
+                console.error("Error al consumir la primera API:", error);
+                return [];
+            }
+        }
 
-    const productosCarrete = [
-        {id:5, imagenUrl: "./multi/jean_dama_azul.jpeg",  textoProducto: "Jean Dama", descuento: "-10%", talla: "Tallas 26-36", precio: 80000, color: "Azul", genero: "mujer"},
-        {id:6, imagenUrl: "./multi/camisa_azulJean_hombre.jpeg", textoProducto: "Camisa hombre", descuento: "-10%", talla: "Tallas S-XL", precio: 60000, color: "Azul", genero: "hombre"},
-        {id:7, imagenUrl: "./multi/jean_dama.jpeg", textoProducto: "Jean dama", descuento: "-10%", talla: "Tallas 26-36", precio: 80000, color: "Azul", genero: "mujer"},
-        {id:8, imagenUrl: "./multi/camisa_casual_hombre.jpeg", textoProducto: "Camisa casual hombre", descuento: "-10%", talla: "Tallas S-XL", precio: 75000, color: "Colores variados", genero: "hombre"},
-        {id:9, imagenUrl: "./multi/camisa_unisex.jpeg", textoProducto: "Camisa unisex", descuento: "-10%", talla: "Tallas S-XL", precio: 78000, color: "Colores variados", genero: "mujer"}
-    ];
+        async function consumirApiProductosCarrete() {
+            try {
+                const respuesta = await fetch('./js/productosCarrete.json');
+                const datos = await respuesta.json();
+                return datos;
+            } catch (error) {
+                console.error("Error al consumir la segunda API:", error);
+                return [];
+            }
+        }
 
-    function generarHTMLProducto(producto) {
+        const [productos, productosCarrete] = await Promise.all([consumirApiProductos(), consumirApiProductosCarrete()]);
+
+        function generarHTMLProducto(producto) {
         const precioConAumento = Math.round(producto.precio * 1.10);
 
         return `
@@ -37,76 +47,79 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 </div>
             </div>
-        `;
-    };
+            `;
+        };
 
-    function generarHTMLProductosCarrete(producto) {
+        function generarHTMLProductosCarrete(producto) {
         const precioConAumento = Math.round(producto.precio * 1.10);
 
-        return `
-            <div class="card" style="width: 18rem;">
-                <img src="${producto.imagenUrl}" class="card-img-top" alt="${producto.textoProducto}">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.textoProducto}</h5>
-                    <h6>${producto.descuento}</h6>
-                    <p class="card-text">${producto.talla}</p>
-                    <p class="card-text">${producto.color}</p>
-                    <div class="seccionPrecios">
-                        <p class="textoTachado">$${precioConAumento}</p>
-                        <p class="card-textPrice">$${producto.precio}</p>
+            return `
+                <div class="card" style="width: 18rem;">
+                    <img src="${producto.imagenUrl}" class="card-img-top" alt="${producto.textoProducto}">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.textoProducto}</h5>
+                        <h6>${producto.descuento}</h6>
+                        <p class="card-text">${producto.talla}</p>
+                        <p class="card-text">${producto.color}</p>
+                        <div class="seccionPrecios">
+                            <p class="textoTachado">$${precioConAumento}</p>
+                            <p class="card-textPrice">$${producto.precio}</p>
+                        </div>
+                        <a href="./pages/detalleProducto.html" class="btn btn-dark">Ver detalle</a>
+                        <a href="./pages/detalleProducto.html" class="btn btn-dark">Añadir al carrito</a>
                     </div>
-                    <a href="./pages/detalleProducto.html" class="btn btn-dark">Ver detalle</a>
-                    <a href="./pages/detalleProducto.html" class="btn btn-dark">Añadir al carrito</a>
                 </div>
-            </div>
-        `;
-    };
+            `;
+        };
     
-    function renderizarProductos(productos, elemento) {
-        const htmlProductos = productos.map(producto => generarHTMLProducto(producto)).join('');
-        elemento.innerHTML = htmlProductos;
-    };
+        function renderizarProductos(productos, elemento) {
+            const htmlProductos = productos.map(producto => generarHTMLProducto(producto)).join('');
+            elemento.innerHTML = htmlProductos;
+        };
     
-    function renderizarProdsCarrete (productos, elemento) {
-        const htmlProductos = productos.map(producto => generarHTMLProductosCarrete(producto)).join('');
-        elemento.innerHTML = htmlProductos;
-    };
+        function renderizarProdsCarrete (productos, elemento) {
+            const htmlProductos = productos.map(producto => generarHTMLProductosCarrete(producto)).join('');
+            elemento.innerHTML = htmlProductos;
+        };
+        
+        renderizarProductos(productos, document.getElementById('listaProductosPagPpal'));
+        renderizarProdsCarrete(productosCarrete, document.getElementById('carrete_recomendados'));
+
+        const prodsPagPpal = productos.concat(productosCarrete); 
     
-    renderizarProductos(productos, document.getElementById('listaProductosPagPpal'));
+        let prodsFiltrados = []; 
+        
+        function filtrarPorGenero(genero) {
+            return prodsPagPpal.filter(producto => producto.genero === genero);
+        }
+        
+        function guardarProductosFiltradosLS(prodsFiltrados) {
+            localStorage.setItem("productosFiltrados", JSON.stringify(prodsFiltrados));
+        }
 
-    renderizarProdsCarrete(productosCarrete, document.getElementById('carrete_recomendados'));
+        function actualizarProdsFiltYSaveLS(genero) {
+            prodsFiltrados = filtrarPorGenero(genero);
+            guardarProductosFiltradosLS(prodsFiltrados);
+        }
 
-    const prodsPagPpal = productos.concat(productosCarrete); 
+        function abrirPagDetalle () {
+            window.open('../pages/detalleProducto.html')
+        }
 
-    let prodsFiltrados = []; 
-
-    function filtrarPorGenero(genero) {
-        return prodsPagPpal.filter(producto => producto.genero === genero);
+        function accionesProd() {
+            const genero = this.dataset.genre;
+            actualizarProdsFiltYSaveLS(genero);
+            console.log(prodsFiltrados);
+            setTimeout(abrirPagDetalle, 1000);
+        }
+        
+        const botonFiltroMujer = document.getElementById('botonFiltroMujer');
+        const botonFiltroHombre = document.getElementById('botonFiltroHombre');
+        
+        botonFiltroMujer.addEventListener("click", accionesProd);
+        botonFiltroHombre.addEventListener("click", accionesProd);
+    
+    } catch (error) {
+        console.error("Error al cargar la página:", error);
     }
-    
-    function guardarProductosFiltradosLS(prodsFiltrados) {
-        localStorage.setItem("productosFiltrados", JSON.stringify(prodsFiltrados));
-    }
-    
-    function actualizarProdsFiltYSaveLS(genero) {
-        prodsFiltrados = filtrarPorGenero(genero);
-        guardarProductosFiltradosLS(prodsFiltrados);
-    }
-
-    function abrirPagDetalle () {
-        window.open('../pages/detalleProducto.html')
-    }
-
-    function accionesProd() {
-        const genero = this.dataset.genre;
-        actualizarProdsFiltYSaveLS(genero);
-        console.log(prodsFiltrados);
-        setTimeout(abrirPagDetalle, 1000);
-    }
-    
-    const botonFiltroMujer = document.getElementById('botonFiltroMujer');
-    const botonFiltroHombre = document.getElementById('botonFiltroHombre');
-    
-    botonFiltroMujer.addEventListener("click", accionesProd);
-    botonFiltroHombre.addEventListener("click", accionesProd);
 });
