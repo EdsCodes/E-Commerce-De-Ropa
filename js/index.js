2a7ec86eb6db7c3206a8c517d37b94585407924f
edscodes <edscodes@gmail.com>
2024-03-21 08:25:10 -0500

    correccion de errores

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     async function consumirApiProductosCarrete() {
         try {
-            const respuesta = await fetch('.gt/js/productosCarrete.json');
+            const respuesta = await fetch('./js/productosCarrete.json');
             const datos = await respuesta.json();
             return datos;
         } catch (error) {
